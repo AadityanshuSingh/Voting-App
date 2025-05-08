7ec3a1a9d3e9e1fec7d64cf481460a8c5c1f28bc
Aadityanshu Singh <aadityanshu.singh.che22@itbhu.ac.in>
2025-05-09 00:52:34 +0530

    Corrected Port logic in server

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ wss.on("connection", (socket) => {
     console.log("Client Disconnected");
   });
 });
-
-httpServer.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+httpServer.listen(PORT, () => {
   console.log(`App is running successfully at 4000`);
 });
